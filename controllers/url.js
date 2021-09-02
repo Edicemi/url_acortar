ecdbc0e4934764d33c9e03b6e352fdb5805d08a9
Edicemi <victoriataiwo1998@gmail.com>
2021-09-02 17:26:10 +0100

    Url Shortner

--- controllers/url.js
+++ controllers/url.js
@@ -1,37 +1,37 @@
 const Url = require('../models/url-model');
 const shortId = require('short-id');
-const isValidURL = require('../lib/utils');
+// const isValidURL = require('../lib/utils');
+// const isValidURL = require('url-validation');
 require('dotenv').config();
 
 const urlShort = async(req, res) => {
-    const fullUrl = req.body;
+    const { fullUrl } = req.body;
     const baseUrl = process.env.BASE_URL;
-    const urlId = shortId.generate();
-    if (isValidURL(fullUrl)) {
-        console.log(fullUrl)
-        try {
-            let url = awaitUrl.findOne({ fullUrl });
-            if (url) {
-                res.json(url);
-            } else {
-                const shortUrl = `${baseUrl}/${urlId}`;
-
-                url = new Url({
-                    fullUrl,
-                    shortUrl,
-                    urlId,
-                    date: new Date(),
-                });
-                await url.save();
-                res.json(url);
-            }
-        } catch (err) {
-            console.log(err);
-            res.status(500).json('Server Error');
+    const urlId = shortId.generate()
+    console.log(fullUrl)
+        // if (await isValidURL(fullUrl)) {
+    try {
+        let url = await Url.findOne({ fullUrl });
+        if (url) {
+            res.json(url);
+        } else {
+            const shortUrl = `${baseUrl}/${urlId}`;
+            url = new Url({
+                fullUrl,
+                shortUrl,
+                urlId,
+                date: new Date(),
+            });
+            await url.save();
+            res.json(url);
         }
-    } else {
-        res.status(400).json('Invalid Original Url');
+    } catch (err) {
+        console.log(err);
+        res.status(500).json('Server Error');
     }
+    // } else {
+    //     res.status(400).json('Invalid Original Url');
+    // }
 };
 
 const getUrl = async(req, res) => {
@@ -45,7 +45,7 @@ const getUrl = async(req, res) => {
         } else res.status(404).json('Not Found');
     } catch (err) {
         console.log(err);
-        res.status(500).json('Server Error');
+        res.status(500).json('Server error z');
     }
 }
 
